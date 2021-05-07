da51ea6787a410f83754f8a248447e19a898a6c0
Gabriel Molopo <7737@univision.local>
2021-05-07 16:05:48 +0200

    created terms of use page

---
+++ terms_of_use.php
@@ -0,0 +1,82 @@
+<?php 
+  $current_page = "";
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php include('includes/header.php');?>
+    <title>QVC</title>
+</head>
+
+<body>
+
+    <?php include('includes/nav.php');?>
+    <?php include('includes/second_nav.php');?>
+
+
+    <div>
+        <div class="jumbotron-fluid myx-0" id="jumbotron" style="background-image: url(images/background/back.jpg);">
+            <br>
+            <div class="jumbotron-block container">
+                <p class="landingHeading">CREATING <br> MOMENTS OF <br> SHEER DELIGHT <br> AND A LIFETIME <br> OF
+                    MEMORIES</p>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- page content here -->
+    <div class="container pt-4">
+        <div class="row">
+            <h1 class="grey">QUALITY VACATION CLUB (QVC) WEBSITE TERMS OF USE</h1><br>
+            <h2 class="grey">Legal Notices<br>
+                Last Reviewed September 2015</h2>
+            <br><br><br><br>
+
+            <p><i>The terms "QVC", "us" or "we" refers to the owner of the website whose registered offices is at Mooikloof Office Park East, corner of Atterbury and Jolify Main Road, Mooikloof, Pretoria, 0059.  Our company registration number is 2007/001866/08. The term "you" refers to the user or viewer of our website.</i></p>
+            <h2 class="grey">Your access to and use of this site is subject to the following terms and conditions, and all applicable South African laws.</h2>
+            <br><br>
+            <p>By accessing and using this site, you accept the following terms and conditions, without limitation or qualification:</p>
+           
+            <ul class="indiResorts">
+                <li>All information contained in this site is for general information purposes only and is subject to change without notice.  You acknowledge that by visiting the site from time to time, you will become bound to the current version of these terms and, unless stated in the current version, all previous versions will be superseded by the current version.  You are responsible for reviewing the current version on each visit to the website.</li>
+                <li>The contents of this site, including any text, images, audio, video, html code, buttons etc. and their arrangement are the property of <strong>Quality Vacation Club (Pty) Ltd</strong> and you may not copy, reproduce or use any such material without our prior written consent.</li>
+                <li>Links on this site may lead to services or sites not operated by us. These links are provided as a convenience to you.  We do not monitor these sites and cannot make representations or warranties regarding the correctness, accuracy, performance or quality of the contents. Your use of these linked sites is subject to the applicable policies and terms and conditions of the specific site.</li>
+                <li>These terms will in all respect be governed and construed in accordance with the laws of the Republic of South Africa.  You submit to the non-exclusive jurisdiction of the Courts of the Republic of South Africa.</li>
+                <li>These terms constitute the entire agreement between the parties in respect of the subject matter of these Terms.</li>
+            </ul>
+        </div>
+
+    </div>
+
+    </div>
+
+    <?php include('includes/holiday_category.php');?>
+
+    <?php include('includes/bottom_nav.php'); ?>
+
+    <?php include('includes/notice_block.php'); ?>
+
+    <?php include('includes/cookie_banner.php'); ?>
+
+    <?php include('includes/footer.php'); ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
