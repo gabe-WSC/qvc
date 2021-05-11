04c7e65bc3bc2ae95d3471903e2eb021a05dbcf3
Gabriel <gabriel@wildsidecreative.co.za>
2021-05-11 10:56:15 +0200

    privacy_policy text fix

--- privacy_policy.php
+++ privacy_policy.php
@@ -17,7 +17,7 @@
 
 
     <div>
-        <div class="jumbotron-fluid myx-0" id="jumbotron" style="background-image: url(images/background/back3.jpg);">
+        <div class="jumbotron-fluid myx-0" id="jumbotron" style="background-image: url(images/background/back.jpg);">
             <br>
             <div class="jumbotron-block container">
                 <p class="landingHeading">CREATING <br> MOMENTS OF <br> SHEER DELIGHT <br> AND A LIFETIME <br> OF
@@ -30,55 +30,89 @@
     <!-- page content here -->
     <div class="container pt-4">
         <div class="row">
-        <h1 class="grey">QUALITY VACATION CLUB (QVC) PRIVACY POLICY</h1>
-        <h2 class="grey">Legal Notices<br>
-                        	Last review August 2020</h2>
-                            <p></p><br><br>
-    
-                            <h2 class="grey">PRIVACY</h2>
-                       	   <p>As an organisation we are committed to protecting  your privacy. We take all reasonable measures, in accordance with legislation  and this policy, to protect personal information which is collected, possessed  and retained when you use our website. </p>
-                           <p>This Privacy  Policy governs the manner in which QVC (hereinafter referred to as <strong>“we”, “our”</strong>) <a name="_Hlk47099491">will process your personal information collected  electronically when you use our website</a>. This policy can be found at: <a href="https://www.qvc.co.za/privacy_policy.php" class="purpleLinks">https://www.qvc.co.za/privacy_policy.php</a></p>
-                           
-<h2 class="grey">OUR OBLIGATIONS IN TERMS OF APPLICABLE LEGISLATION</h2>
-                         	<p>This Privacy Policy will be governed by, construed and interpreted in accordance with enacted Privacy Laws of the Republic of South Africa.</p>
-							 
-                            <h2 class="grey">HOW DO WE COLLECT YOUR PERSONAL INFORMATION? </h2>
-                         	<p>Your personal information may be collected while using this website, when you contact us, when you complete an application form, when you check in at your destination or when you make use of the services of one of our affiliated business partners. </p>
-							 
-                           <h2 class="grey">USE OF YOUR PERSONAL INFORMATION </h2>
-                         	<p>
-						   We need to collect your personal information in order to enable you to make use of our website and all products and services offered on our website. We use your personal information in the ordinary course of our business and to send you marketing material relating to any other similar sites that we, and our business partners are involved with. You can unsubscribe at any time and we will thereafter no longer use your personal information. We will not disclose your personal information to unrelated third parties or third parties who we do not have business relations with. </p>
+            <h1 class="grey">QUALITY VACATION CLUB (QVC) PRIVACY POLICY</h1>
+            <h2 class="grey w-100">Legal Notices<br> Last review August 2020</h2>
 
-						   <h2 class="grey">ACCESS TO AND CORRECTION OF PERSONAL INFORMATION </h2>
-                         	<p>
-							You may access your personal information that we hold by sending an email to <a href="mailto:webmaster@oaks.co.za" class="purpleLinks">webmaster@oaks.co.za</a>. In the event that you want to update or correct your personal information held by us, you can send an email to <a href="mailto:webmaster@oaks.co.za" class="purpleLinks">webmaster@oaks.co.za</a></p>
-                         	
-<h2 class="grey">PROTECTION OF YOUR PERSONAL INFORMATION </h2>
-                         	<p>
-							Whilst we cannot guarantee absolute security of your personal information, we take all reasonable steps and measures to keep it secure from loss, misuse or unauthorised alteration. Our database has built-in safeguards to ensure the privacy and confidentiality of your information. We do not have control over the websites of affiliated parties or sites linked into from this site and you must therefore refer to the privacy policies of any such parties to see how they protect your personal information. </p>
+            
+            <h2 class="grey py-3">PRIVACY</h2>
+            <p>As an organisation we are committed to protecting your privacy. We take all reasonable measures, in
+                accordance with legislation and this policy, to protect personal information which is collected,
+                possessed and retained when you use our website. </p>
+            <p>This Privacy Policy governs the manner in which QVC (hereinafter referred to as <strong>“we”,
+                    “our”</strong>) <a name="_Hlk47099491">will process your personal information collected
+                    electronically when you use our website</a>. This policy can be found at: <a
+                    href="https://www.qvc.co.za/privacy_policy.php"
+                    class="purpleLinks">https://www.qvc.co.za/privacy_policy.php</a></p>
 
-							<h2 class="grey">USE OF “COOKIES”</h2>
-                         	<p>
-							We sometimes use “cookies”. “Cookies” are programs that we transfer to your computer’s hard drive to personalise your experience on this website. In this way, the next time you visit, we will know who you are and that you have visited this website before. This will enable us to enhance your browsing experience. You can set your browser to notify you if “cookies” are to be transferred or to reject “cookies” but this may prevent your use of some of the web pages. </p>
+            <h2 class="grey pb-3">OUR OBLIGATIONS IN TERMS OF APPLICABLE LEGISLATION</h2>
+            <p>This Privacy Policy will be governed by, construed and interpreted in accordance with enacted Privacy
+                Laws of the Republic of South Africa.</p>
 
-							<h2 class="grey">E-MAIL MONITORING </h2>
-                         	<p>
-							We provide email addresses on the website which enable you to contact us. Forms that you complete via this website will also be transmitted via email. In order to check for viruses and to monitor service we reserve the right to monitor and inspect all material and information transmitted over our system.</p>
+            <h2 class="grey pb-3">HOW DO WE COLLECT YOUR PERSONAL INFORMATION? </h2>
+            <p>Your personal information may be collected while using this website, when you contact us, when you
+                complete an application form, when you check in at your destination or when you make use of the services
+                of one of our affiliated business partners. </p>
 
-							<h2 class="grey">CHANGES TO THIS PRIVACY POLICY </h2>
-                         	<p>
-							We reserve the right, in our sole discretion, to amend this Privacy Policy from time to time. You agree to review the Privacy Policy whenever you visit this website for any such amendments. The amended version of the Privacy Policy shall supersede and replace all previous versions thereof. </p>
+            <h2 class="grey pb-3">USE OF YOUR PERSONAL INFORMATION </h2>
+            <p>
+                We need to collect your personal information in order to enable you to make use of our website and all
+                products and services offered on our website. We use your personal information in the ordinary course of
+                our business and to send you marketing material relating to any other similar sites that we, and our
+                business partners are involved with. You can unsubscribe at any time and we will thereafter no longer
+                use your personal information. We will not disclose your personal information to unrelated third parties
+                or third parties who we do not have business relations with. </p>
 
-							<h2 class="grey">CONTRAVENTION OF THIS PRIVACY POLICY</h2>
-                         	<p>
-							To the extent that a court has jurisdiction over any dispute which may arise out of or in connection with this Privacy Policy, you agree to the exclusive jurisdiction of the Information Regulator and/or South African Courts.</p>
+            <h2 class="grey pb-3">ACCESS TO AND CORRECTION OF PERSONAL INFORMATION </h2>
+            <p>
+                You may access your personal information that we hold by sending an email to <a
+                    href="mailto:webmaster@oaks.co.za" class="purpleLinks">webmaster@oaks.co.za</a>. In the event that
+                you want to update or correct your personal information held by us, you can send an email to <a
+                    href="mailto:webmaster@oaks.co.za" class="purpleLinks">webmaster@oaks.co.za</a></p>
+
+            <h2 class="grey pb-3">PROTECTION OF YOUR PERSONAL INFORMATION </h2>
+            <p>
+                Whilst we cannot guarantee absolute security of your personal information, we take all reasonable steps
+                and measures to keep it secure from loss, misuse or unauthorised alteration. Our database has built-in
+                safeguards to ensure the privacy and confidentiality of your information. We do not have control over
+                the websites of affiliated parties or sites linked into from this site and you must therefore refer to
+                the privacy policies of any such parties to see how they protect your personal information. </p>
+
+            <h2 class="grey pb-3">USE OF “COOKIES”</h2>
+            <p>
+                We sometimes use “cookies”. “Cookies” are programs that we transfer to your computer’s hard drive to
+                personalise your experience on this website. In this way, the next time you visit, we will know who you
+                are and that you have visited this website before. This will enable us to enhance your browsing
+                experience. You can set your browser to notify you if “cookies” are to be transferred or to reject
+                “cookies” but this may prevent your use of some of the web pages. </p>
+
+            <h2 class="grey pb-3">E-MAIL MONITORING </h2>
+            <p>
+                We provide email addresses on the website which enable you to contact us. Forms that you complete via
+                this website will also be transmitted via email. In order to check for viruses and to monitor service we
+                reserve the right to monitor and inspect all material and information transmitted over our system.</p>
+
+            <h2 class="grey pb-3">CHANGES TO THIS PRIVACY POLICY </h2>
+            <p>
+                We reserve the right, in our sole discretion, to amend this Privacy Policy from time to time. You agree
+                to review the Privacy Policy whenever you visit this website for any such amendments. The amended
+                version of the Privacy Policy shall supersede and replace all previous versions thereof. </p>
+
+            <h2 class="grey pb-3">CONTRAVENTION OF THIS PRIVACY POLICY</h2>
+            <p>
+                To the extent that a court has jurisdiction over any dispute which may arise out of or in connection
+                with this Privacy Policy, you agree to the exclusive jurisdiction of the Information Regulator and/or
+                South African Courts.</p>
 
 
-							<p><em>Disclaimer<br>
-							Take note that the information contained in this document does not constitute legal advice on the subject of Personal Information Protection. Should you require advise, please contact your attorney or legal consultant. Should you elect to act and / or rely on the information herein contained, we indemnify ourselves against any loss or damage that may arise, whether direct or consequential.  </em></p>
+            <p><em>Disclaimer<br>
+                    Take note that the information contained in this document does not constitute legal advice on the
+                    subject of Personal Information Protection. Should you require advise, please contact your attorney
+                    or legal consultant. Should you elect to act and / or rely on the information herein contained, we
+                    indemnify ourselves against any loss or damage that may arise, whether direct or consequential.
+                </em></p>
 
         </div>
-        
+
     </div>
 
     </div>
